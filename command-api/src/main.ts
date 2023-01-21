77037adfd6f03487cfaa4c8c79b3f8e6f2e2c649
Vladimir Kunin <v.b.kunin@gmail.com>
2023-01-22 01:08:49 +0500

    Add CORS support to the Command API

--- command-api/src/main.ts
+++ command-api/src/main.ts
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 import { router } from './router.js'
 
 dotenv.config()
 
 const app = express()
-
+app.use(cors({
+  "origin": "*",
+  "methods": "POST,PATCH"
+}))
 app.use(express.json())
 app.use('/v1', router)
 
